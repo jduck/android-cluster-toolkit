2b570da23cd81d9b2a995655379f0bcca9ca01e7
Joshua J. Drake <github.jdrake@qoop.org>
2014-04-04 01:59:58 -0500

    Look for "devices-orig.rb" in the cluster home

--- init.rb
+++ init.rb
@@ -20,20 +20,22 @@ $:.unshift(File.dirname(bfn))
 # get a list of devices via 'adb devices'
 require 'madb'
 new = adb_scan()
+$stderr.puts "[*] Found #{new.length} device#{plural(new.length)} via 'adb devices'"
 
-$stderr.puts "[*] Found #{new.length} device#{plural(new.length)}!"
 
-if File.exists? 'devices-orig.rb'
+orig_devices = File.join(File.dirname(bfn), 'devices-orig.rb')
+
+if File.exists? orig_devices
   $stderr.puts "[!] devices-orig.rb exists! rm it to start over"
   exit(1)
 end
 
 template = nil
-File.open('devices-orig.rb.sample', 'rb') { |f|
+File.open("#{orig_devices}.sample", 'rb') { |f|
   template = f.read
 }
 
-File.open('devices-orig.rb', 'wb') { |f|
+File.open(orig_devices, 'wb') { |f|
   f.puts template.split(/^=end$/).first + "=end"
   new.each { |dev|
     f.puts %Q|
